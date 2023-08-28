f715cc89d296507941cea55d922cf3831262009b
mnanjwan <mnanjwan@gmail.com>
2023-08-28 21:19:34 +0100

    added edits to datatables final inJesus name

--- resources/views/admin/user/approve.blade.php
+++ resources/views/admin/user/approve.blade.php
@@ -115,7 +115,8 @@
                                 ================================= -->
 
     @push('table-scripts')
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" ></script>
         <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
         <script>
             // jQuery.noConflict();
